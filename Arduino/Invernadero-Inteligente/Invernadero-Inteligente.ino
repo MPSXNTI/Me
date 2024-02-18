d55b4a945fb0d14daf3e2d71fbd586ded696f872
MPSXNTI <153019351+MPSXNTI@users.noreply.github.com>
2024-02-18 14:27:52 -0300

    Update Invernadero-Inteligente.ino

--- Arduino/Invernadero-Inteligente/Invernadero-Inteligente.ino
+++ Arduino/Invernadero-Inteligente/Invernadero-Inteligente.ino
@@ -38,88 +38,83 @@ void setup() {
 }
 
 void loop() {
-  iniciarTrigger();
-  obtenerDatos();
-  controlLeds();
-  controlMotor();
-  actualizarPantalla();
-  Serial.println("Humedad: " + String(humedad) + "%");
-  Serial.println("Luminicidad: " + String(luminicidad));
-  Serial.println("Distancia: " + String(distancia) + "cm");
+  mainArduino();
   delay(1000);
 }
 
-void iniciarTrigger(){
-  digitalWrite(Trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(Trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(Trig, LOW);
-}
+void mainArduino() {
+  // iniciarTrigger
+    digitalWrite(Trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trig, LOW);
 
-void obtenerDatos() {
-  humedad = map(analogRead(SenH), 0, 1023, 117, 0);
-  luminicidad = analogRead(SenF);
-  tiempo = pulseIn(Echo, HIGH);
-  distancia = (tiempo * 0.000001 * 34000.0)/ 2;
-}
+  // obtenerDatos
+    humedad = map(analogRead(SenH), 0, 1023, 117, 0);
+    Serial.println("Humedad: " + String(humedad) + "%");
 
-void controlLeds() {
-  digitalWrite(LedRed, LOW);
-  digitalWrite(LedYellow, LOW);
-  digitalWrite(LedBlue, LOW);
-  
-  if (humedad < 30) {
-    digitalWrite(LedRed, HIGH);
-  } else if (humedad < 70) {
-    digitalWrite(LedYellow, HIGH);
-  } else {
-    digitalWrite(LedBlue, HIGH);
-  }
+    luminicidad = analogRead(SenF);
+    Serial.println("Luminicidad: " + String(luminicidad));
 
-  if (luminicidad < 300) {
-    digitalWrite(LedWhite, HIGH);
-  } else {
-    digitalWrite(LedWhite, LOW);
-  }
-}
+    tiempo = pulseIn(Echo, HIGH);
+    distancia = (tiempo * 0.000001 * 34000.0)/ 2;
+    Serial.println("Distancia: " + String(distancia) + "cm");
 
-void controlMotor() {
-  if (luminicidad < 300) {
-    motor.write(0);
-  } else if (distancia < 10) {
-    motor.write(0);
-    tone(Piezo, 523, 200);
-  } else {
-    motor.write(180);
-  }
-}
+  // controlLeds
+    digitalWrite(LedRed, LOW);
+    digitalWrite(LedYellow, LOW);
+    digitalWrite(LedBlue, LOW);
 
-void actualizarPantalla() {
-  if (luminicidad < 300) {
-    lcd.setCursor(0,0);
-    lcd.print("Noche");
-  } else {
-    lcd.setCursor(0,0);
-    lcd.print("Dia  ");
-  }
-  
-  lcd.setCursor(6,0);
-  lcd.print("//");
-  
-  int angulo = motor.read();
-  if (angulo == 0) {
-    lcd.setCursor(9,0);
-    lcd.print("Cerrado");
-  } else {
-    lcd.setCursor(9,0);
-    lcd.print("Abierto");
-  }
-  
-  lcd.setCursor(0,1);
-  lcd.print("Humedad: " + String(humedad) + "%     ");
-  delay(1000);
-  lcd.setCursor(0,1);
-  lcd.print("Distancia: " + String(distancia) + "cm  ");
-  delay(1000);
+    if (humedad < 30) {
+      digitalWrite(LedRed, HIGH);
+    } else if (humedad < 70) {
+      digitalWrite(LedYellow, HIGH);
+    } else {
+      digitalWrite(LedBlue, HIGH);
+    }
+
+    if (luminicidad < 300) {
+      digitalWrite(LedWhite, HIGH);
+    } else {
+      digitalWrite(LedWhite, LOW);
+    }
+
+  // controlMotor
+    if (luminicidad < 300) {
+      motor.write(0);
+    } else if (distancia < 10) {
+      motor.write(0);
+      tone(Piezo, 523, 200);
+    } else {
+      motor.write(180);
+    }
+
+  // actualizarPantalla
+    if (luminicidad < 300) {
+      lcd.setCursor(0,0);
+      lcd.print("Noche");
+    } else {
+      lcd.setCursor(0,0);
+      lcd.print("Dia  ");
+    }
+
+    lcd.setCursor(6,0);
+    lcd.print("//");
+
+    int angulo = motor.read();
+    if (angulo == 0) {
+      lcd.setCursor(9,0);
+      lcd.print("Cerrado");
+    } else {
+      lcd.setCursor(9,0);
+      lcd.print("Abierto");
+    }
+
+    lcd.setCursor(0,1);
+    lcd.print("Humedad: " + String(humedad) + "%     ");
+    delay(1000);
+    lcd.setCursor(0,1);
+    lcd.print("Distancia: " + String(distancia) + "cm  ");
+    delay(1000);
 }
