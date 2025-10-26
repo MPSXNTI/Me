8cf9f7e588904fbbc4724d6c187e3c467dc933f4
MPSXNTI <153019351+MPSXNTI@users.noreply.github.com>
2025-10-26 13:19:27 -0300

    Delete delete/poo-mini directory

--- delete/poo-mini/python/vehiculos/auto.py
+++
@@ -1,5 +0,0 @@
-from .vehiculo import Vehiculo
-
-class Auto(Vehiculo):
-    def mover(self) -> str:
-        return f"Auto {self.marca} avanza a {self.velocidad} km/h."
--- delete/poo-mini/python/vehiculos/bicicleta.py
+++
@@ -1,5 +0,0 @@
-from .vehiculo import Vehiculo
-
-class Bicicleta(Vehiculo):
-    def mover(self) -> str:
-        return f"Bici {self.marca} pedalea a {self.velocidad} km/h."
